3781308b542a72b9e33fa580ba544b8e7b165ca6
Sandhya Dixit <sandhyavdixit@gmail.com>
2020-04-11 14:38:58 +0530

    First cut of Qualification details.

---
+++ src/_shared/FormStructure/QulificationDetails.js
@@ -0,0 +1,114 @@
+export const DataStructure = [
+    {
+        name: 'qualificationLevel',
+        label: 'Qualification Level',
+        type: 'select',
+        config: {
+            options: [
+              { value: 'tenthLevel', displayValue: 'Tenth Level' },
+              { value: 'twelthLevel', displayValue: 'Twelth Level' },
+              { value: 'diploma', displayValue: 'Diploma' },
+              { value: 'graduateLevel', displayValue: 'Graduate Level' },
+              { value: 'postGraduateLevel', displayValue: 'Post Graduate Level' },
+              { value: 'doctrate', displayValue: 'Doctrate' }
+            ]
+          },
+          value: 'tenthLevel',
+          validation: {},
+          valid: true
+    },
+    {
+        name: 'universityBoard',
+        label: 'University / Board',
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: 'University / Board'
+        },
+        value: '',
+        validation: {
+          required: true
+        }, 
+        valid: false,
+        touched: false
+    },
+    {
+        name: 'yearOfPass',
+        label: 'Year of Pass',
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: 'Year of pass e.g. 2020'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 4,
+          maxLength: 4,
+          isNumeric: true
+        }, 
+        valid: false,
+        touched: false
+    },
+    {
+        name: 'resultClassification',
+        label: 'Result Classification',
+        type: 'select',
+        config: {
+            options: [
+              { value: 'third', displayValue: 'Third Class' },
+              { value: 'second', displayValue: 'Second Class' },
+              { value: 'first', displayValue: 'First Class' },
+              { value: 'distiction', displayValue: 'Distiction' },
+            ]
+          },
+          value: 'third',
+          validation: {},
+          valid: true
+    },
+    {
+        name: 'institute',
+        label: 'Institute',
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: 'Name of the institute'
+        },
+        value: '',
+        validation: {
+          required: true
+        }, 
+        valid: false,
+        touched: false
+    },
+    {
+        name: 'subject',
+        label: 'Subject',
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: 'Enter Subject'
+        },
+        value: '',
+        validation: {
+          required: true
+        }, 
+        valid: false,
+        touched: false
+    },
+    {
+        name: 'percentageOfMark',
+        label: 'Percentage of Mark',
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: 'Percentage of Mark'
+        },
+        value: '',
+        validation: {
+          required: true
+        }, 
+        valid: false,
+        touched: false
+    }
+]
--- src/components/Qualification_Details/index.js
+++ src/components/Qualification_Details/index.js
@@ -1,8 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DataStructure } from '../../_shared/FormStructure/QulificationDetails';
+import { Input } from '../../_shared/Input';
+import { Modal } from '../../_shared/Modal';
+import { CheckValidity } from '../../_utils/CheckValidity'; 
 
 
 export const QualificationDetails = () => {
+    const initialdata = {
+        qualificationLevel: 'tenth',
+        university: '',
+        yearOfPass: '',
+        resultClassification: 'third',
+        institute: '',
+        subject: '',
+        percentage: '',
+    }
+
+    const initialState = {
+        dataStructure : DataStructure,
+        qualification : initialdata,
+        qualifications : []
+    }
+
+    const [ state, setState] = useState(initialState);
+    const [ showModal, setShowModal] = useState(false);
+    const updateState = data => setState(prevState => ({ ...prevState, ...data }));
+    
+    const onClick = () =>{
+        setShowModal(true);
+    }
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    }
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+
+        console.log(name);
+
+        const updatedQualification = {
+            ...state.qualification,
+            [name]: value
+        };
+
+        const updatedDataStructure = state.dataStructure.map(field => {
+            if (field.name === name) {
+                field.touched = true;
+                field.value = value;
+
+                field.valid = CheckValidity(
+                    value,
+                    field.validation
+                );
+            }
+            return field;
+        });
+
+        setState({
+            qualification: updatedQualification,
+            dataStructure: updatedDataStructure
+        });
+    }
+
+    const formElement = (
+        <div className='form'>
+          { state.dataStructure.map(eachData => (
+            <Input
+                key            ={eachData.name}
+                name           ={eachData.name}
+                label          ={eachData.label}
+                type           ={eachData.type}
+                config         ={eachData.config}
+                value          ={eachData.value}
+                invalid        ={!eachData.valid}
+                shouldValidate ={eachData.validation}
+                touched        ={eachData.touched}
+                changed ={e => onChangeHandler(e)}
+            />            
+          ))}
+        </div>
+      );
+
     return (
-        <p>QualificationDetails</p>
+        <div>
+            <button onClick={onClick}>Add</button>
+
+            <Modal 
+                show= {showModal}
+                onClose= {toggleModal}
+                title= 'Add Educational Details'>
+
+                    {formElement}
+
+            </Modal>
+
+        </div>
      );
 }
